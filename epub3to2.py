7088c8eeba1fe2ce1c2e3c86ce15cdea6b283b20
Giuseppe <giuseppe.pesce@outlook.it>
2019-04-01 00:53:55 +0200

    rename files + unzip files

--- epub3to2.py
+++ epub3to2.py
@@ -1,7 +1,15 @@
-import os, fnmatch
+import glob, os
+import zipfile
 
-listOfFiles = os.listdir('.')  
-pattern = "*.epub"  
-for entry in listOfFiles:  
-    if fnmatch.fnmatch(entry, pattern):
-        os.rename(entry, entry+'zip')
+folder = "."
+for filename in glob.iglob(os.path.join(folder, '*.epub')):
+     #rename all file from epub to zip
+     os.rename(filename, filename[:-4] + '.zip')
+     
+for filename in glob.iglob(os.path.join(folder, '*.zip')):
+        print filename
+        zip_ref = zipfile.ZipFile(filename, 'r')
+        os.makedirs(filename[:-4])
+        zip_ref.extractall(filename[:-4])
+        zip_ref.close()
+
